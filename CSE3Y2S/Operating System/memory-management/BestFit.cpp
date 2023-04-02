74fd133a27738f915e806d6c6f4af1d0b12e32a7
Aminul Islam <68509378+Aminul264@users.noreply.github.com>
2023-04-02 22:03:20 +0600

    Create BestFit.cpp

---
+++ CSE3Y2S/Operating System/memory-management/BestFit.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+
+
+void solve()
+{
+	 cin>>n>>m;
+	 
+    vector<int>process(n);
+    vector<pair<int,int> > frame(n);
+    int check[m]={0};
+    
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>process[i];
+    }
+    for(int i=0;i<m;i++)
+    {
+        int x;
+        cin>>x;
+        frame[i]={x,i};
+
+    }
+    sort(frame.begin(),frame.end());
+
+    bool flag=true;
+    for(int i=0;i<n;i++)
+    {
+        flag=true;
+        for(int j=0;j<m;j++)
+        {
+            if(check[j]==0)
+            {
+                if(process[i]<=frame[j].first)
+                {
+                    check[j]=1;
+                    flag=false;
+                    cout<<i+1<<" ";
+                    cout<<process[i]<<" ";
+                    cout<<frame[j].second+1<<endl;
+                    break;
+                }
+            }
+        }
+
+        if(flag)
+        {
+            cout<<i+1<<" ";
+            cout<<process[i]<<" ";
+            cout<<"Not allocated"<<endl;
+        }
+    }
+
+}
+
+int main( )
+{
+
+    cout<<"process no  ";
+    cout<<"process size  ";
+    cout<<"frame number  "<<endl;;
+    solve();
+
+
+}
