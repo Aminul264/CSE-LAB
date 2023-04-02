b23c3a6aaf305dd539cbe03542fe74770f175d4c
Aminul Islam <68509378+Aminul264@users.noreply.github.com>
2023-04-02 22:56:52 +0600

    Create page-fault-fifo.cpp

---
+++ CSE3Y2S/Operating System/page-replacement/page-fault-fifo.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,frame_size=3;
+void solve()
+{
+    cin>>n;
+    vector<int>frame(n);
+    int mp[n]={0};
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>frame[i];
+    }
+    queue<int>q;
+
+    int cnt=0;
+    for(int i=0;i<n;i++)
+    {
+        if(mp[frame[i]]==0)
+        {
+            cnt++;
+            if(q.size()<frame_size)
+            {
+                q.push(frame[i]);
+                mp[frame[i]]=1;
+            }
+            else
+            {
+                mp[q.front()]=0;
+                q.pop();
+                q.push(frame[i]);
+            }
+        }
+
+    }
+
+    cout<<"Page fault : "<<cnt<<endl;
+
+}
+int main()
+{
+   solve();
+}
