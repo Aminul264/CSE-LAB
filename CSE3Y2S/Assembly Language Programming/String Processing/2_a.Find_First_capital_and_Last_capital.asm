65a53d319280eebbb7f1ef35d2ef3f9e8e914855
Aminul Islam <68509378+Aminul264@users.noreply.github.com>
2023-07-05 22:18:41 +0600

    Update 2_a.Find_First_capital_and_Last_capital.asm

--- CSE3Y2S/Assembly Language Programming/String Processing/2_a.Find_First_capital_and_Last_capital.asm
+++ CSE3Y2S/Assembly Language Programming/String Processing/2_a.Find_First_capital_and_Last_capital.asm
@@ -1,130 +1,95 @@
 
 ;find first capital and last capital of a string
 
-
-
-.model small
-.stack 100h
-.data  
-
- msg1 db       'Enter a string  : $'
- msg2 db 10,13,'First capital   : $'
- msg3 db 10,13,'Last capital    : $' 
- msg4 db 10,13,'No capitals     :  $'
  
- first1 db 91;say first capital=91
- last1  db 64;say last capital =64
+ ;FIND OUT FIRST CAPITAL AND LAST CAPITAL 
+.MODEL SMALL 
+.STACK 100H
+.DATA
  
- str db 100 dup ('$')   
-     
-     
-.code
-main proc   
-    
-    mov ax,@data
-    mov ds,ax   
-    
-    ;print msg1
-    lea dx,msg1
-    mov ah,9
-    int 21h
-    
-    ;take input
-    mov si,0
-input:  
-
-      mov ah,1
-      int 21h
-      cmp al,13
-      je end_input  
-      mov str[si],al
-      inc si 
-      jmp input 
-      
-end_input:
-      mov si,0
-      mov bl,first1
-      mov cl,last1
-      
-      
-process:    
-      cmp str[si],'$'
-      je end_process 
-      
-      cmp str[si],'A'
-      jl next_elem
-      
-      cmp str[si],'Z'
-      jg next_elem
-       
-      
-      ;check first capital
-      cmp str[si],bl
-      jl first
-   
-      ;check last capital
-      cmp str[si],cl
-      jg last  
-      
-      
-      inc si
-      jmp process
-
-
-first: 
-     mov bl,str[si]
-     inc si
-     jmp process  
-     
-last:
-    mov cl,str[si]
-    inc si
-    jmp process
+ MSG0 DB 10,13,'ENTER ACSTRING : $'
+ MSG1 DB 10,13,'FIRST CAPITAL : $' 
+ MSG2 DB 10,13,'LAST CAPITAL  : $'
+ MSG3 DB 10,13,'NO CAPITAL  : $'
+ 
+ ARR DB 100 DUP('$') 
+ FC  DB 91   ;FC=FIRST CAPITAL
+ LC  DB 64  ;LC=LAST CAPITAL
+ 
+ 
+.CODE
           
-next_elem:
-      inc si
-      jmp process
+               
+          
+MAIN PROC  
+    
+    
+    MOV AX,@DATA
+    MOV DS,AX 
+    
+    
+    LEA DX,MSG0
+    MOV AH,9
+    INT 21H
+       
+    MOV AH,1 
+    MOV SI,-1
+    
+    INPUT:
+      INT 21H
+      CMP AL,13;IF ENTER KEY IN PRESSED
+      JE END_INPUT 
       
+      CMP AL,'Z'
+      JG INPUT 
+      CMP AL,FC
+      JL UPDATE_FC 
+      CMP AL,LC
+      JG UPDATE_LC
+      
+      JMP INPUT 
+    
+    UPDATE_FC: 
+       MOV FC,AL
+       JMP INPUT
+       
+    UPDATE_LC:
+       MOV LC,AL
+       JMP INPUT
+      
+    END_INPUT:
+    
+    
+    CMP LC,64
+    JNE PRINT
+    
+    MOV AH,9
+    LEA DX,MSG3
+    INT 21H 
+    JMP EXIT
+    
+    
+    PRINT:
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H
+    MOV AH,2
+    MOV DL,FC
+    INT 21H 
+    
+    MOV AH,9
+    LEA DX,MSG2
+    INT 21H
+    MOV AH,2
+    MOV DL,LC
+    INT 21H
 
-
-end_process: 
-
-;check is no capital exist?
-cmp bl,91;64means same as before (defined)
-jnz print_capitals
-
-lea dx,msg4
-mov ah,9
-int 21h
-jmp exit
- 
- 
-print_capitals:
-;print first capital  
-
-  lea dx,msg2
-  mov ah,9
-  int 21h
-
-  mov dl,bl
-  mov ah,2
-  int 21h 
-  
-  
-;print last capital  
-
-  lea dx,msg3
-  mov ah,9
-  int 21h 
-  mov dl,cl
-  mov ah,2
-  int 21h
-
+       
 
     
-    exit:
-    mov ah,4ch
-    int 21h
+    EXIT:
+    MOV AH,4CH
+    INT 21H
     
-    main endp
-end main
+    MAIN ENDP
+END MAIN
