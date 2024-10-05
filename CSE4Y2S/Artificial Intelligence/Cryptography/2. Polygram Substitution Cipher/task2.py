ca60b4017c8c25f7df94bc19c729d05d932104a9
Aminul Islam <68509378+Aminul264@users.noreply.github.com>
2024-10-05 18:03:10 +0600

    Add files via upload

--- CSE4Y2S/Artificial Intelligence/Cryptography/2. Polygram Substitution Cipher/task2.py
+++ CSE4Y2S/Artificial Intelligence/Cryptography/2. Polygram Substitution Cipher/task2.py
@@ -1,77 +1,58 @@
 
+en_dictionary = {}
+dec_dictionary = {}
 
-# if __name__=='__main__':
-#     with open('input.txt','r') as infile:
-#         plaintext = infile.read()
-#         print(plaintext)
-
-# import math
-
-# def encrypt(msg, width):
-#     # Add trailing spaces to make the message length a multiple of width
-#     trailing_space = width - len(msg) % width
-#     if trailing_space != width:
-#         msg += ' ' * trailing_space
-
-#     cipher = ""
-#     for i in range(width):
-#         for j in range(i, len(msg), width):
-#             cipher += msg[j]
-    
-#     return cipher
-
-# def decrypt(cipher, width):
-#     rows = math.ceil(len(cipher) / width)
-#     msg = ""
-    
-#     for i in range(rows):
-#         for j in range(i, len(cipher), rows):
-#             msg += cipher[j]
-    
-#     # Remove trailing spaces
-#     msg = msg.rstrip()
-    
-#     return msg
-
-# def main():
-#     # Read width and message from the input file
-#     with open('in.txt', 'r') as infile:
-#         width = int(infile.readline().strip())
-#         msg = infile.read().replace('\n', '')
-    
-#     # Encrypt and decrypt
-#     cipher = encrypt(msg, width)
-#     decrypted_msg = decrypt(cipher, width)
-    
-#     # Print and write results to the output file
-#     with open('out.txt', 'w') as outfile:
-#         outfile.write(f"Original message : {decrypted_msg}\n")
-#         outfile.write(f"Cipher message : {cipher}\n")
-
-#     print(f"Original message : {decrypted_msg}")
-#     print(f"Cipher message : {cipher}")
-
-def encrypt(plaintext, width):
-    # to ensure its length is a multiple of the specified width
-    padding =width - len(plaintext)%width
-    if padding!=width:
-        plaintext +=' '*padding
+def create_dictionary(dictionary):
+    for line in dictionary:
+        w1,w2 = line.strip().split(' ')
+        en_dictionary[w1] = w2
+        dec_dictionary[w2] = w1
+def encrypt(msg,block_size):
     cipher = ""
-    for i in range(width):
-        for j in range(i,len(plaintext),width):
-            cipher += plaintext[j]
-    
+    word=""
+    for ch in msg:
+        if ch.isalpha():
+            word += ch.upper()
+            # if len(word) == block_size:
+            if word in  en_dictionary:
+                cipher += en_dictionary[word]
+                word = ""
+        else:
+            cipher +=ch
     return cipher
+    
+def decrypt(cipher,block_size):
+    decipher = ""
+    word= ""
+    for ch in cipher:
+        if ch.isalpha():
+            word+=ch.upper()
+            # if len(word)==block_size:
+            if word in dec_dictionary:
+                decipher+=dec_dictionary[word]
+                word = ""
+        else:
+            decipher+=ch    
+    return decipher
 
 if __name__ == "__main__":
     
     with open('input.txt') as infile:
-        # Removes any leading and trailing whitespace characters, including newlines, from the read line
-        width =int(infile.readline().strip())
-        # Removes all newline characters from the message.
-        plaintext = infile.read().replace('\n','')
-        cipher = encrypt(plaintext, width)
-        print(cipher)
+        block_size =int(infile.readline().strip())
+        msg = infile.readline().strip()
+    with open('dictionary.txt', 'r') as infile:
+        dictionary = infile.readlines()
+    create_dictionary(dictionary)
+    # print(en_dictionary)
+    # print(dec_dictionary)
+    cipher = encrypt(msg,block_size)
+    print(f'cipher: {cipher}')
+    decipher = decrypt(cipher,block_size)
+    print(f'decipher: {decipher}')
+    
+    
+        
+    
         
         
         
